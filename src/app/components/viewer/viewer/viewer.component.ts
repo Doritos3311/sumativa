71415ff7064d4435e3e7e5e47cbe892d34307d2b
dorian.santamaria120 <dorian.santamaria120@gmail.com>
2024-11-20 02:59:46 +0000

    7mo Commit

--- src/app/components/viewer/viewer/viewer.component.ts
+++ src/app/components/viewer/viewer/viewer.component.ts
@@ -3,14 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-viewer',
   standalone: true, // Asegúrate de que esto esté presente
-  template: `
-    <div>
-      <h3>Datos Ingresados:</h3>
-      <p><strong>Email:</strong> {{ email }}</p>
-      <p><strong>Contraseña:</strong> {{ password }}</p>
-      <p><strong>Texto:</strong> {{ text }}</p>
-    </div>
-  `,
+  templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.css']
 })
 export class ViewerComponent {
